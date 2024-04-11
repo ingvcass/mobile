9473592bfcd6b4941a67eaf3650dcc141d41dca7
ingvcass <113540264+ingvcass@users.noreply.github.com>
2024-04-11 18:16:28 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,36 @@ from flask import Flask, request
 #from flack import request
 import math
 
-app = Flask('myapp')
+app = Flask(__name__)
+@app.route('/')
+def start():
+    start = """
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lab11</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <p>Через / указать нужное выражение и числа (пример: /sun/10.0/20.0)</p>
+    <p>1.Сложение sum</p>
+    <p>2.Вычитание min</p>
+    <p>3.Умножение mul</p>
+    <p>4.Деление deli</p>
+    <p>5.Корень sqrt</p>
+    <p>6.Степень ste</p>
+    <p>7.Целочисленное деление cel </p>
+    <p>8.Остаток от деления ost</p>
+    <p>Можно выбрать единицы счисления ?mode= (пример: /sin/30.0?mode=degrees) </p>
+    <p>1.Синус sin</p>
+    <p>2.Косинус cos</p>
+    <p>3.Тангенс tg</p>
+</body>
+</html>
+    """
+    return start
 @app.route('/sum/<float:a>/<float:b>')
 def sum (a,b):
     return str(a+b)
@@ -60,4 +89,4 @@ def cel (a,b):
         return str(a // b)
     else:
         return (str('делитель ноль'))
-app.run(host='0.0.0.0', debug = True)
+#app.run(host='0.0.0.0', debug = True)
